3527598229eeb00baf4d18f1082f9d31811cf68a
Raghav Verma <103280940+Raghavv21@users.noreply.github.com>
2023-05-06 16:56:37 +0530

    Add files via upload

---
+++ HackerRank solution Python/HackerRank solution Python/Mutations.py
@@ -0,0 +1,12 @@
+def mutate_string(string, position, character):
+    n = list(string)
+    n[position] = character
+    string = "".join(n)
+    return string
+
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
